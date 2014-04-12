d6b3b533ec29dd109140853719c636cd767e4b9a
xph906 <xiangpan2011@gmail.com>
2014-04-12 13:49:24 -0500

    modified

--- prototype.js
+++ prototype.js
@@ -16,11 +16,24 @@ function MyObject(value){
 var objRef1 = new MyObject(4);
 var objRef2 = new MyObject(5);
 var prot = MyObject.prototype;
-console.log("objRef1.number: "+objRef1.number);
-console.log("objRef2.number: "+objRef2.number);
-console.log("prot.number: "+prot.number);
+console.log("objRef1.number: "+objRef1.number);    //4
+console.log("objRef2.number: "+objRef2.number);    //5
+console.log("prot.number: "+prot.number);          //undifined
 
 /*
 Now change objRef1 and objRef2's prototypes, 
-if number is in prototype, 
+if number is in prototype, we shouldn't see number properety anymore;
+Otherwise, number property is still there.
+*/
+
+objRef1.prototype = Object.prototype;
+objRef2.prototype = Object.prototype;
+
+console.log("[After Chaning Prototype] objRef1.number: "+objRef1.number);  //4
+console.log("[After Chaning Prototype]objRef2.number: "+objRef2.number);   //5
+
+/*Conclusion:
+	When define an object from a constructor:
+	var obj = new MyObject(number);
+	"this.property" in constructor will set the value in the object, instead of in prototypes
 */
